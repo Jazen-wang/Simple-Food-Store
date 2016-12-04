643478183cfda206920f127121aa764054a5f880
黄潮立 <gavingog@qq.com>
2016-12-04 14:20:06 +0800

    get food logic reset

--- server/control/index.js
+++ server/control/index.js
@@ -39,11 +39,25 @@ module.exports = function (db) {
 
   interface.createUser = createFunc(user, 'User');
   interface.findUser = findFunc(user, 'User');
-  interface.createFood = createFunc(food, 'Food');
-  interface.findFood = findFunc(food, 'Food');
   interface.createOrder = createFunc(order, 'Order');
   interface.findOrder = findFunc(order, 'Order');
+  interface.createFood = createFunc(food, 'Food');
 
+  interface.findFood = function (req, res, next) {
+    let filter = req.query || {};
+    let userVip = false;
+    if (req.session && req.session.user) {
+      userVip = req.session.user.vip;
+    }
+    food.findFood(filter).then((arr)=>{
+      for (var i = 0; i < arr.length; i++) {
+        arr[i].vip = userVip && arr[i].vip;
+      }
+      sendData(res, 200, arr);
+    }).catch((err)=>{
+      sendData(res, 400, err.message);
+    });
+  }
 
   interface.login = function (req, res, next) {
     var loginUser = req.body;
